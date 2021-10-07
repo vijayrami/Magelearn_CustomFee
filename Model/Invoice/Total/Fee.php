7482832ef50b135b5727889b7aa9194ffb6c9eef
Vijay Rami <vijaymrami@gmail.com>
2021-10-07 11:30:53 +0530

    Update Fee.php

--- Model/Invoice/Total/Fee.php
+++ Model/Invoice/Total/Fee.php
@@ -13,12 +13,9 @@ class Fee extends AbstractTotal
     public function collect(\Magento\Sales\Model\Order\Invoice $invoice)
     {
         $invoice->setFee(0);
-        $invoice->setBaseFee(0);
 
         $amount = $invoice->getOrder()->getFee();
         $invoice->setFee($amount);
-        $amount = $invoice->getOrder()->getBaseFee();
-        $invoice->setBaseFee($amount);
 
         $invoice->setGrandTotal($invoice->getGrandTotal() + $invoice->getFee());
         $invoice->setBaseGrandTotal($invoice->getBaseGrandTotal() + $invoice->getFee());
