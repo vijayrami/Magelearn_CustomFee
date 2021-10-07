33e3742f86870dd55a62794addd7a6bd454b2023
Vijay Rami <vijaymrami@gmail.com>
2021-10-07 11:31:15 +0530

    Update Fee.php

--- Model/Creditmemo/Total/Fee.php
+++ Model/Creditmemo/Total/Fee.php
@@ -13,13 +13,9 @@ class Fee extends AbstractTotal
     public function collect(\Magento\Sales\Model\Order\Creditmemo $creditmemo)
     {
         $creditmemo->setFee(0);
-        $creditmemo->setBaseFee(0);
 
         $amount = $creditmemo->getOrder()->getFee();
         $creditmemo->setFee($amount);
-
-        $amount = $creditmemo->getOrder()->getBaseFee();
-        $creditmemo->setBaseFee($amount);
 
         $creditmemo->setGrandTotal($creditmemo->getGrandTotal() + $creditmemo->getFee());
         $creditmemo->setBaseGrandTotal($creditmemo->getBaseGrandTotal() + $creditmemo->getBaseFee());
